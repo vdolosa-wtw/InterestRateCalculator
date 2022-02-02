ea76205d3a933add5f57a8cf35d41faf146acaf9
Virgilio Dolosa <virgilio.dolosa@willistowerswatson.com>
2022-02-02 19:13:49 +0800

    Created scaffolding for the Indentity and removed unnecessary/unsupported features.

---
+++ InterestRateCalculator.WebApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+using System;
+using InterestRateCalculator.WebApp.Data;
+using InterestRateCalculator.WebApp.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(InterestRateCalculator.WebApp.Areas.Identity.IdentityHostingStartup))]
+namespace InterestRateCalculator.WebApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
