b4dccb80bc4aac82e82993da59875b460b2d0b19
Virgilio Dolosa <virgilio.dolosa@willistowerswatson.com>
2022-02-02 23:46:32 +0800

    Added "InterestConfiguration" model for storing configurations.

---
+++ InterestRateCalculator.Domain/Configuration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterestRateCalculator.Domain
+{
+    public class InterestConfiguration
+    {
+        public const double DEFAULT_INTEREST_LOWER = 0.1;
+        public const double DEFAULT_INTEREST_UPPER = 0.5;
+        public const double DEFAULT_INTEREST_INCREMENTAL = 0.2;
+
+        public string ConfigName { get; set; }
+
+        public double Value { get; set; }
+    }
+}
