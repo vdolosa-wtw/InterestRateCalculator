aeca1531ba1e64d23ca94e6f2f572417baeb1e03
Virgilio Dolosa <virgilio.dolosa@willistowerswatson.com>
2022-02-03 23:22:33 +0800

    Added unit test project for the "InterestRateCalculator.WebApp"

---
+++ InterestRateCalculator.WebApp.Test/CalculateInterestControllerTest.cs
@@ -0,0 +1,21 @@
+using InterestRateCalculator.Domain;
+using InterestRateCalculator.WebApp.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace InterestRateCalculator.WebApp.Test
+{
+    [TestClass]
+    public class CalculateInterestControllerTest
+    {
+        [TestMethod]
+        public void CanCallGetFromCalculateInterestController()
+        {
+            var interestCalculator = new Mock<IInterestCalculator>();
+            var calculateInterestController = new CalculateInterestController(interestCalculator.Object);
+            calculateInterestController.Get(0, 0);
+
+            interestCalculator.Verify(calc => calc.Calculate(It.IsAny<decimal>(), It.IsAny<int>()), Times.AtMostOnce());
+        }
+    }
+}
