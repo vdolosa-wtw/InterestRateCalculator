761ed2d235dcb8386f81a636b080bf23fda919e2
Virgilio Dolosa <virgilio.dolosa@willistowerswatson.com>
2022-02-09 00:45:14 +0800

    Removed some built-in sample codes.

--- InterestRateCalculator.WebApp/ClientApp/src/app/app.module.ts
+++ InterestRateCalculator.WebApp/ClientApp/src/app/app.module.ts
@@ -9,8 +9,6 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ApiAuthorizationModule } from 'src/api-authorization/api-authorization.module';
 import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
 import { AuthorizeInterceptor } from 'src/api-authorization/authorize.interceptor';
@@ -22,9 +20,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   declarations: [
     AppComponent,
     NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
+    HomeComponent,    
     InterestRateCalculatorComponent,
     CalculationSessionComponent
   ],
@@ -34,9 +30,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     FormsModule,
     ApiAuthorizationModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent, canActivate: [AuthorizeGuard] },
+      { path: '', component: HomeComponent, pathMatch: 'full' },      
       { path: 'calculate', component: InterestRateCalculatorComponent, canActivate: [AuthorizeGuard] },
       { path: 'sessions', component: CalculationSessionComponent, canActivate: [AuthorizeGuard] }
     ]),
