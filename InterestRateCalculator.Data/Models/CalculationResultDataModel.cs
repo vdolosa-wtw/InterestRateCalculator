cb4609a0c41d7dc6b97d5881ca2f52b842a78a36
Virgilio Dolosa <virgilio.dolosa@willistowerswatson.com>
2022-02-09 16:58:42 +0800

    Moved out the extension codes to a separate file

--- InterestRateCalculator.Data/Models/CalculationResultDataModel.cs
+++ InterestRateCalculator.Data/Models/CalculationResultDataModel.cs
@@ -19,54 +19,5 @@ namespace InterestRateCalculator.DataProvider.Models
         public decimal FutureValue { get; set; }
 
         public CalculationSessionDataModel Session { get; set; }
-    }
-
-    public static class CalculationResultDataModelExtensions
-    {
-        public static CalculationResult ToCalculationResult(this CalculationResultDataModel dataModel)
-        {
-            return new CalculationResult
-            {
-                Year = dataModel.Year,
-                CurrentValue = dataModel.CurrentValue,
-                InterestRate = dataModel.InterestRate,
-                FutureValue = dataModel.FutureValue
-            };
-        }
-
-        public static CalculationResultDataModel ToEntity(this CalculationResult result)
-        {
-            return new CalculationResultDataModel
-            {                
-                Year = result.Year,
-                CurrentValue = result.CurrentValue,
-                InterestRate = result.InterestRate,
-                FutureValue = result.FutureValue
-            };
-        }
-
-        public static ICollection<CalculationResult> ToCalculationResults(this ICollection<CalculationResultDataModel> collection)
-        {
-            var results = new List<CalculationResult>();
-
-            foreach(var item in collection)
-            {
-                results.Add(item.ToCalculationResult());
-            }
-
-            return results;
-        }
-
-        public static ICollection<CalculationResultDataModel> ToEntities(this ICollection<CalculationResult> results)
-        {
-            var entities = new List<CalculationResultDataModel>();
-
-            foreach(var item in results)
-            {
-                entities.Add(item.ToEntity());
-            }
-
-            return entities;
-        }
-    }
+    }    
 }
---
+++ InterestRateCalculator.Data/Models/Extensions.cs
@@ -0,0 +1,68 @@
+﻿using InterestRateCalculator.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterestRateCalculator.DataProvider.Models
+{
+    public static class Extensions
+    {
+        public static CalculationResult ToCalculationResult(this CalculationResultDataModel dataModel)
+        {
+            return new CalculationResult
+            {
+                Year = dataModel.Year,
+                CurrentValue = dataModel.CurrentValue,
+                InterestRate = dataModel.InterestRate,
+                FutureValue = dataModel.FutureValue
+            };
+        }
+
+        public static CalculationResultDataModel ToEntity(this CalculationResult result)
+        {
+            return new CalculationResultDataModel
+            {
+                Year = result.Year,
+                CurrentValue = result.CurrentValue,
+                InterestRate = result.InterestRate,
+                FutureValue = result.FutureValue
+            };
+        }
+
+        public static ICollection<CalculationResult> ToCalculationResults(this ICollection<CalculationResultDataModel> collection)
+        {
+            List<CalculationResult> results = null;
+
+            if (collection != null)
+            {
+                results = new List<CalculationResult>();
+
+                foreach (var item in collection)
+                {
+                    results.Add(item.ToCalculationResult());
+                }
+            }
+
+            return results;
+        }
+
+        public static ICollection<CalculationResultDataModel> ToEntities(this ICollection<CalculationResult> results)
+        {
+            List<CalculationResultDataModel> entities = null;
+
+            if (results != null)
+            {
+                entities = new List<CalculationResultDataModel>();
+
+                foreach (var item in results)
+                {
+                    entities.Add(item.ToEntity());
+                }
+            }
+
+            return entities;
+        }
+    }
+}
